15b7eb0bfeac0539e9dac62662baa066e3f07f69
Ceren Battal <cerenbattal@Cerens-MacBook-Air.local>
2023-04-29 14:23:14 +0300

    feat: design updates on image cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #333;
+  color: #fff;
+  padding: 1rem;
+`;
+
+const Logo = styled.h1`
+  font-size: 24px;
+  margin: 0;
+`;
+
+const Navigation = styled.nav`
+  display: flex;
+`;
+
+const NavItem = styled.a`
+  color: #fff;
+  text-decoration: none;
+  margin-left: 1rem;
+`;
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderContainer>
+      <Logo>Infinite-fav!</Logo>
+      <Navigation>
+        <NavItem href="#">Favorites</NavItem>
+        <NavItem href="#">Profile</NavItem>
+      </Navigation>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,8 +1,62 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, {useState, useEffect, useRef} from 'react';
+import styled from "styled-components";
+
+const GalleryImageWrapper = styled.div`
+  position: relative;
+  grid-row-end: span ${(props) => (props.spans)};
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  transform: translate(-50%, -50%);
+  display: ${(props) => (props.hovered ? "flex" : "none")};
+  background-color: rgba(128,128,128,0.75);
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
+`;
+
+const Caption = styled.p`
+  font-size: 16px;
+  margin-bottom: 1rem;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  border: 2px solid white;
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 8rem;
+  cursor: pointer;
+`;
+
+const FavoritedButton = styled.button`
+  background-color: white;
+  border: 2px solid white;
+  color: rgba(0, 0, 0, 0.5);
+  padding: 0.5rem 1rem;
+  border-radius: 8rem;
+  cursor: pointer;
+`;
 
 const ImageCard = ({ image }) => {
+  const [isHovered, setIsHovered] = useState(true);
   const [spans, setSpans] = useState(0);
   const imageRef = useRef();
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(true);
+  };
 
   useEffect(() => {
     imageRef.current.addEventListener('load', handleSpans);
@@ -15,14 +69,25 @@ const ImageCard = ({ image }) => {
   };
 
   return (
-    <div style={{ gridRowEnd: `span ${spans}` }}>
+    <GalleryImageWrapper
+      hovered={isHovered}
+      spans={spans}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <img
         ref={imageRef}
         alt={image.title}
         loading="lazy"
         src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
       />
-    </div>
+      {isHovered && (
+        <Overlay hovered={isHovered}>
+          <Caption>{image.title}</Caption>
+          <FavoritedButton>Favorited!</FavoritedButton>
+        </Overlay>
+      )}
+    </GalleryImageWrapper>
   )
 }
 
